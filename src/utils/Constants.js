14f2dd06ed9f62b6e9e590f9a64e439390cf5549
adkood <ashutoshnegi195@gmail.com>
2023-12-23 20:35:22 +0530

    constant

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = process.env.NODE_ENV === "development" ? 'http://localhost:8000/api' : 'https://dashboard-server-ghnx.onrender.com/api';
-export const API_URL = 'https://dashboard-server-ghnx.onrender.com/api';
+export const API_URL = process.env.NODE_ENV === "development" ? 'http://localhost:8000/api' : 'https://dashboard-server-ghnx.onrender.com/api';
+// export const API_URL = 'https://dashboard-server-ghnx.onrender.com/api';
 export const DEFAULT_CHART_DATA = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     datasets: [{
